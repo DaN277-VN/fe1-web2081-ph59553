43cdd8abf3830afd6586343f571d1009b28411a3
Duc <ducbkbk2211@gmail.com>
2026-03-13 12:41:01 +0700

    lab2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { Contact } from './pages/contact/contact';
+import { Home } from './pages/home/home';
+import { Products } from './pages/products/products';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'about', component: About },
+  { path: 'contact', component: Contact },
+  { path: 'products', component: Products },
+];
